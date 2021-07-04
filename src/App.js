6607cc63ff32784e4cd99523990f58707c5fe927
oktyajipangestu <oktyajipangestu@gmail.com>
2021-07-04 13:39:13 +0700

    menambahkan carousel

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import {
   DropdownButton,
   Figure,
   Breadcrumb,
+  Carousel,
 } from "react-bootstrap";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Row className="bg-dark" md={4}>
+        <Row className="bg-dark">
           <Col>
             <DropdownButton id="dropdown-basic-button" title="Pilih Bahasa">
               <Dropdown.Item href="#/action-2">Indonesia</Dropdown.Item>
@@ -44,6 +45,50 @@ class App extends Component {
             </Breadcrumb>
           </Col>
         </Row>
+
+        <br />
+        <Row>
+          <Col md={{ span: 6, offset: 3 }}>
+            <Carousel>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://images.performgroup.com/di/library/GOAL/f3/33/england-celebrate-vs-montenegro_e97kv5anpqlq1gr9nc2qaz4ko.jpg?t=-1232521935&quality=60&w=1600"
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3>Inggris Mau Jadi Tim Terbaik</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://images.performgroup.com/di/library/GOAL/30/f0/neymar-psg-paris-saint-germain-2018-19_gmakk5891n9c19udcwofmrsgc.jpg?t=-1258742791&quality=60&w=1600"
+                  alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                  <h3>Neymar Bahas Kontrak Baru Di Paris Saint-Germain</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="https://images.performgroup.com/di/library/GOAL/63/7f/sani-rizki-fauzi-indonesia-u-22_4yztc4nehkt61fa7omuyagf71.jpg?t=351764441&quality=60&w=1600"
+                  alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                  <h3>
+                    Sani Rizki Fauzi Tegaskan Mental Pemain Timnas Indonesia
+                    U-23 Tetap bagus
+                  </h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </Col>
+        </Row>
+
       </div>
     );
   }
