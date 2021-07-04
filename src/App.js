1483de0d29b5eee124a4b912f1ad101a2c459a73
oktyajipangestu <oktyajipangestu@gmail.com>
2021-07-04 12:50:22 +0700

    pembuatan struktur file dan instalasi bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import { Alert } from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hallo Tugas 8</h1>
+        <Alert variant="success">Berhasil</Alert>
+      </div>
+    );
+  }
 }
 
 export default App;
